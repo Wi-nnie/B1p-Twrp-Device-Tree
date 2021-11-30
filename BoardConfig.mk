85b254af7b145fdd3fbbec7dab3c811d8cf22660
Wi-nnie <94724733+Wi-nnie@users.noreply.github.com>
2021-11-30 15:16:57 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/tecno/TECNO-B1p
+DEVICE_PATH := device/tecno/B1p
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
